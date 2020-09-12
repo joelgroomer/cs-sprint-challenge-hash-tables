6f08ab61117ca1ea801b44f389ef1d49832ead5d
Joel <joelgroomer@gmail.com>
2020-09-11 20:59:01 -0700

    ex1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,16 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    table = {}
+
+    for i, weight in enumerate(weights):
+        complement = limit - weight
+        if complement in table:
+            return (i, table[complement])
+        else:
+            table[weight] = i
 
     return None
+
+
+if __name__ == "__main__":
+    print(get_indices_of_item_weights(
+        [12, 6, 7, 14, 19, 3, 0, 25, 40], length=9, limit=7))
