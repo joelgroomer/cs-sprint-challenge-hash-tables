b138cef2de801cf4c32b82ced94b8f35281b42d0
Joel <joelgroomer@gmail.com>
2020-09-11 19:26:22 -0700

    ex4 complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,18 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    d = {}
+
+    positives = [v for v in a if v >= 0]
+    negatives = [v for v in a if v < 0]
+
+    for val in positives:
+        d[val] = False
+
+    for val in negatives:
+        if val * -1 in d:
+            d[val * -1] = True
+
+    result = [v[0] for v in d.items() if v[1] == True]
 
     return result
 
